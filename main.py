5c1f515fc0e4d8c00ff2ae80c4ec91d445021db8
AhmedovAF <sybot703@gmail.com>
2024-10-31 19:43:51 +0300

    feat: add multiplication and division f

--- main.py
+++ main.py
@@ -2,8 +2,15 @@
 # main.py
 def add(a, b):
     return a + b
-<<<<<<< HEAD
-=======
 def subtract(a, b):
     return a - b
->>>>>>> feature/subtr
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+def mod(a, b):
+    return a % b
