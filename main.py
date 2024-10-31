1f5fd981424f0c714df332cfe6327d6b3c9f3d30
AhmedovAF <sybot703@gmail.com>
2024-10-31 19:50:25 +0300

    feat: add mem ops

--- main.py
+++ main.py
@@ -21,3 +21,15 @@ def divide(a, b):
 
 def mod(a, b):
     return a % b
+class Memory:
+    def __init__(self):
+        self.memory = 0
+
+    def m_add(self, value):
+        self.memory += value
+
+    def m_clear(self):
+        self.memory = 0
+
+    def m_recall(self):
+        return self.memory
